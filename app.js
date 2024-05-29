1c0fd8345eb791cd0588502a063eb37d92235219
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-05-29 13:03:41 +0530

    hi

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const expressSession=require('express-session');
+const passport=require('passport'); 
+const usersRouter=require('./models/user');
+const path=require('path')
 const port = process.env.PORT || 3000;
 
+app.set('view engine','ejs');
+app.use(expressSession({
+  saveUninitialized:false,
+  resave:false,
+  secret:'hey hey hey'
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.serializeUser(usersRouter.serializeUser());
+// passport.deserializeUser(usersRouter.deserializeUser());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-app.get('/:name',(req,res)=>{
-  res.send(`Hi ${req.params.name}`)
+app.post('/create',(req,res)=>{
+  
 })
 
 app.listen(port, () => {
---
+++ models/user.js
@@ -0,0 +1,13 @@
+// const mongoose=require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/project').then(
+//     console.log('connected')
+// )
+
+// const userSchema=mongoose.Schema({
+//     username:String,
+//     password:String,
+//     fullname:String,
+// })
+
+// module.exports=mongoose.model('user',userSchema);
