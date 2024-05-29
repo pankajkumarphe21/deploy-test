fe47ebbbfbdc33437051ac9841448fcd332be4fd
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-05-29 14:31:26 +0530

    hi2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
-  res.send('index');
+  res.render('index');
 });
 
 app.listen(port, () => {
